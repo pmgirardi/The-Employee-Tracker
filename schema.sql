b5787c8bf9f7633723d8f4d38fd813df3a7ae6bc
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-03-15 17:11:29 -0700

    inserted data for tables in seeds.sql file

--- schema.sql
+++ schema.sql
@@ -9,32 +9,34 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
--- Creates the 'department' table
+-- Creates the 'department' table and adding contraints 
 
 CREATE TABLE departments (
-    id INT NOT NULL, 
-    name VARCHAR(30) NOT NULL, 
-    PRIMARY KEY (id)
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(30) NOT NULL
 );
 
--- Creates the 'roles' table
+-- Creates the 'roles' table and adding contraints 
 
 CREATE TABLE roles (
-    id INT NOT NULL,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL, 
     salary DECIMAL(10,2) NOT NULL, 
-    department_id INT UNSIGNED NOT NULL REFERENCES departments(id), 
-    PRIMARY KEY (id)
+    department_id INT UNSIGNED NOT NULL,
+      INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
--- Creates the `employee` table
+-- Creates the `employee` table and adding contraints 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT UNSIGNED NOT NULL REFERENCES roles(id),
-    manager_id INT UNSIGNED NOT NULL REFERENCES manager(id),
-    PRIMARY KEY (id)
+    role_id INT UNSIGNED NOT NULL, INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
 -- `employee`
